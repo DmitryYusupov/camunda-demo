38e2777816210f348a5478ac126f934a114ea34e
dyusupov <dmitry.yusupov@infobip.com>
2021-01-03 13:56:15 +0300

    Optimized imports

--- src/main/kotlin/ru/yusdm/training/camunda/common/longrunningflow/LongRunningFlowDemoController.kt
+++ src/main/kotlin/ru/yusdm/training/camunda/common/longrunningflow/LongRunningFlowDemoController.kt
@@ -1,6 +1,5 @@
 package ru.yusdm.training.camunda.common.longrunningflow
 
-import org.camunda.bpm.engine.RuntimeService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController

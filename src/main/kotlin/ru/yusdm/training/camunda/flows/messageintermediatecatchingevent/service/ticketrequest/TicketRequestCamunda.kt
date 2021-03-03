c652a9a6d9a5271bf8d5aa310c18cb21f74d1d51
dyusupov <dmitry.yusupov@infobip.com>
2021-03-03 19:31:21 +0300

    Updated ticket request/response

--- src/main/kotlin/ru/yusdm/training/camunda/flows/messageintermediatecatchingevent/service/ticketrequest/TicketRequestCamunda.kt
+++ src/main/kotlin/ru/yusdm/training/camunda/flows/messageintermediatecatchingevent/service/ticketrequest/TicketRequestCamunda.kt
@@ -3,11 +3,13 @@ package ru.yusdm.training.camunda.flows.messageintermediatecatchingevent.service
 import org.camunda.bpm.engine.delegate.DelegateExecution
 import org.camunda.bpm.engine.delegate.JavaDelegate
 import org.springframework.stereotype.Component;
+import ru.yusdm.training.camunda.common.solutions.camunda.get
+import ru.yusdm.training.camunda.flows.messageintermediatecatchingevent.api.FlowExecutor
 
 @Component("messageFlowTicketRequestCamunda")
 class TicketRequestCamunda(private val ticketRequestService: TicketRequestService) : JavaDelegate {
 
     override fun execute(execution: DelegateExecution) {
-        ticketRequestService.execute()
+        ticketRequestService.execute(execution[FlowExecutor.USER_ID]!!)
     }
 }
